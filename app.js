965d1833c9ddd4193b62c91cfc072c2a5b7f9492
ClementBuchanan <clement.buchanan@gmail.com>
2020-12-04 22:22:21 -0800

    updated completed question 3

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
+//Write your code here
 function multiply(a, b) { //eslint-disable-line
   var myProd = a * b;
   var message = `The product of ${a} and ${b} is ${myProd}.`;
@@ -40,16 +40,19 @@ function multiply(a, b) { //eslint-disable-line
 
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the 
+- first element is the sum of those three numbers, 
+- the second element is the product of those three numbers,  and the 
+- third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
-Third element: "4 and 7 and 5 sum to 16."
-Fourth element: "The product of 4 and 7 and 5 is 140."
+- Third element: "4 and 7 and 5 sum to 16."
+- Fourth element: "The product of 4 and 7 and 5 is 140."
 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
@@ -57,11 +60,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var mySum1 = sum(a, b)[0];
+  var mySum2 = sum(c, mySum1)[0];
+  var message1 = `${a} and ${b} and ${c} sum to ${mySum2}.`;
+  var myProd1 = multiply(a, b)[0];
+  var myProd2 = multiply(c, myProd1)[0];
+  var message2 = `The product of ${a} and ${b} and ${c} is ${myProd2}.`;
+  //console.log(message2);
+  return [mySum2, myProd2, message1, message2];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
