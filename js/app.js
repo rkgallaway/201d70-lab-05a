59fd60f83b486cccfa5059f3bfff69b32603f3c0
Adrian Milsap <adrian@hirobius.com>
2021-01-23 15:59:38 -0800

    problem 2 test successful

--- js/app.js
+++ js/app.js
@@ -28,15 +28,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// // Write your code here
-// function multiply(a, b) { //eslint-disable-line
-//   let product = 5 * 9;
-//   let message = 'The product of 5 and 9 is 45.';
-//   return [product, message];
-// }
+// Write your code here
+function multiply(a, b) { //eslint-disable-line
+  let product = 5 * 9;
+  let message = 'The product of 5 and 9 is 45.';
+  return [product, message];
+}
 
-// // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5, 9);
+// Here is the test for multiply(); uncomment it to run it
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
